a2f780ce1fe2814b84a903abf56c77fc4ff22ca5
makcyt01 <makcyt01@gmail.com>
2017-11-05 14:07:26 +0300

    Add files via upload
    update

--- src/main/resources/6.sql
+++ src/main/resources/6.sql
@@ -1,3 +1,3 @@
 SELECT avg(salary) as average
 FROM developers inner join projects on projects.company_id = developers.company_id
-WHERE projects.company_id = developers.company_id and (select min(cost) from projects)
+WHERE projects.cost = (select min(cost) from projects)
