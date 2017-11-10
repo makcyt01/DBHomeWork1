405c30cfab3f8aea11649d4f489beef515f10c23
makcyt01 <makcyt01@gmail.com>
2017-11-10 10:59:40 +0300

    Add files via upload
    update 2.sql

--- src/main/resources/2.sql
+++ src/main/resources/2.sql
@@ -1,11 +1,5 @@
-create table if not exists pmax (project varchar(50), total int);
-insert into pmax
-  select project, sum(salary) as total from developers
-    inner join companies on companies.id = developers.company_id
-    inner join projects on projects.company_id = companies.id
-  group by projects.id;
-
-select project from pmax
-where total = (select max(total) from pmax);
-
-drop table pmax
+SELECT project, sum(salary) as total
+FROM projects, developers
+WHERE projects.company_id = developers.company_id
+GROUP BY projects.project
+ORDER BY total desc limit 1
